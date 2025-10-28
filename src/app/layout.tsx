4864c156e414f5996f8621ebeeb28211b8ae78b5
Stefano Santomauro <sfn.santomauro@gmail.com>
2025-10-28 15:33:20 -0700

    Added .ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Financial Freedom - Smart Financial Calculators',
   description: 'Free financial calculators to help you make smarter decisions',
+  icons: {
+    icon: '/financial_freedom.ico',
+  },
 }
 
 export default function RootLayout({

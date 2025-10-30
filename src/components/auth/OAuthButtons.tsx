e24bc6c988ec777685ca67ee736d861a0b0be7f3
Stefano Santomauro <sfn.santomauro@gmail.com>
2025-10-30 10:43:33 -0700

    Hide Apple Sign In button

--- src/components/auth/OAuthButtons.tsx
+++ src/components/auth/OAuthButtons.tsx
@@ -62,7 +62,9 @@ export function OAuthButtons() {
         Continue with Google
       </AuthButton>
 
-      <AuthButton
+      {/* Apple Sign In - Hidden for now (requires $99/year Apple Developer Program) */}
+      {/* Uncomment to re-enable when Apple Developer account is set up */}
+      {/* <AuthButton
         variant="outline"
         onClick={() => handleOAuth('apple')}
         isLoading={isLoading === 'apple'}
@@ -72,7 +74,7 @@ export function OAuthButtons() {
           <path d="M17.05 20.28c-.98.95-2.05.88-3.08.4-1.09-.5-2.08-.48-3.24 0-1.44.62-2.2.44-3.06-.4C2.79 15.25 3.51 7.59 9.05 7.31c1.35.07 2.29.74 3.08.8 1.18-.24 2.31-.93 3.57-.84 1.51.12 2.65.72 3.4 1.8-3.12 1.87-2.38 5.98.48 7.13-.57 1.5-1.31 2.99-2.54 4.09l.01-.01zM12.03 7.25c-.15-2.23 1.66-4.07 3.74-4.25.29 2.58-2.34 4.5-3.74 4.25z"/>
         </svg>
         Continue with Apple
-      </AuthButton>
+      </AuthButton> */}
     </div>
   )
 }
